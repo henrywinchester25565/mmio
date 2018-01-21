20a535af0a9705d0460b496eb1cb3284baabe605
henrywinchester25565 <henrywinchester25565@googlemail.com>
2018-01-21 17:24:54 +0000

    Fixed bug with one time events preventing second events from running.

--- src/entity.js
+++ src/entity.js
@@ -6,6 +6,8 @@ console.log("Loaded: entity.js");
 
 //REQUIREMENTS
 const $VECTOR = require('./general').vector;
+const $BOUNDS = require('./bounds');
+const $EVENTS = require('./events.js');
 
 //BASE CLASS
 class Entity {
@@ -13,34 +15,88 @@ class Entity {
     constructor (x, y) {
         this.x = x;
         this.y = y;
+
+        this.events = new $EVENTS.handler();
+        this.changed = false;
+        this.collides = false;
+
         this.type = 'entity'; //Type of entity
+        this.bounds = new $BOUNDS.bounds.point(this.x, this.y);
+    }
+
+    //Wrapper for death event
+    onKill (handler) {
+        this.events.on('killed', handler);
+    }
+
+    kill () {
+        this.events.emit('killed');
+    }
+
+    scrape () {
+        return {
+            x: this.x,
+            y: this.y,
+            type: this.type,
+            bounds: this.bounds.scrape()
+        }
+    }
+
+}
+
+class Wall extends Entity {
+
+    constructor (x, y, w, h) {
+        super(x, y);
+        this.w = w;
+        this.h = h;
+
+        this.collides = true;
+
+        this.type = 'wall';
+        this.bounds = new $BOUNDS.bounds.box(this.x, this.y, w, h);
+    }
+
+    //Get the minimum data for client transfer
+    scrape () {
+        return {
+            x: this.x,
+            y: this.y,
+            w: this.w,
+            h: this.h,
+            type: this.type,
+            bounds: this.bounds.scrape()
+        }
+    }
+
+}
+
+class StaticLight extends Entity {
+
+    constructor (x, y, color, intensity) {
+        super (x, y);
+
+        this.color = color;
+        this.intensity = intensity;
     }
 
 }
 
 //ALL ENTITIES
-const $ENTS = {
-    entity: Entity
+const $ENTITIES = {
+    entity: Entity,
+    wall: Wall,
+    light_s: StaticLight
 };
 
 //CHECK IF ENTITY
 const $IS_ENTITY = function (entity) {
-    return $ENTS[entity.type] !== undefined;
-};
-
-//ALL ENTITIES
-const $CONTROLLERS = {};
-
-//CHECK IF CONTROLLER
-const $IS_CONTROLLER = function (controller) {
-    return $CONTROLLERS[controller.type] !== undefined;
+    return $ENTITIES[entity.type] !== undefined;
 };
 
 //EXPORTS
 exports.ents = $ENTITIES;
 exports.isEnt = $IS_ENTITY;
-exports.controllers = $CONTROLLERS;
-exports.isController = $IS_CONTROLLER;
 
 //NOTES
 //Commented out because it's useful, but needs to be rewritten into above
