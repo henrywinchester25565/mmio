c2822716d354ab5090a6f4883ef85ffddc7edc86
henrywinchester25565 <henrywinchester25565@googlemail.com>
2018-01-21 17:28:51 +0000

    Boundary testing for narrow phase. AABB boundaries improved.

--- src/bounds.js
+++ src/bounds.js
@@ -7,6 +7,73 @@ console.log("Loaded: bounds.js");
 //REQUIREMENTS
 const $VECTOR = require ('./general').vector;
 
+//TEST IF IN BOUNDS
+const $IN_BOUNDS = function (a, b) {
+    switch (b.type) {
+        case 'point': return a.inBoundsPoint(b);
+        case 'box': return a.inBoundsBox(b);
+        default: return false;
+    }
+};
+
+//POINT
+class Point {
+
+    constructor (x, y) {
+        this.type = 'point';
+        this.update(x, y);
+    }
+
+    update (x, y) {
+        this.xMin = x;
+        this.xMax = x;
+        this.yMin = y;
+        this.yMax = y;
+    }
+
+    //For another point
+    inBoundsPoint (point) {
+        return this.xMin === point.xMin && this.yMin === point.yMin;
+    }
+
+    //For a line
+
+    //For a box
+    inBoundsBox (bounds) {
+        let x = this.xMin;
+        let y = this.yMin;
+        return (x >= bounds.xMin && x <= bounds.xMax) && (y >= bounds.yMin && y <= bounds.yMax);
+    }
+
+    //TODO For a circle
+    inBoundsCircle () {}
+
+    inBounds (bounds) {
+        return $IN_BOUNDS(this, bounds);
+    }
+
+    //Get the minimum data for client transfer
+    scrape () {
+        return {
+            xMin: this.x,
+            yMin: this.y,
+            xMax: this.x,
+            yMax: this.y,
+            type: this.type
+        };
+    }
+
+}
+
+//LINES
+class Line {
+
+    constructor () {
+
+    }
+
+}
+
 //AXIS ALIGNED BOUNDING BOX
 class Box {
 
@@ -20,57 +87,39 @@ class Box {
 
     update (x, y) {
         this.xMin = x;
-        this.xMax = x + w;
+        this.xMax = x + this.w;
         this.yMin = y;
-        this.yMax = y + h;
-    }
-
-    stretch (x, y) {
-        let xO = this.xMin + this.w * 0.5;
-        let yO = this.yMin + this.h * 0.5;
-
-        this.w = this.w * x;
-        this.h = this.h * y;
-
-        this.xMin = xO - this.w * 0.5;
-        this.xMax = xO + this.w * 0.5;
-        this.yMin = yO - this.h * 0.5;
-        this.yMax = xO + this.h * 0.5;
+        this.yMax = y + this.h;
     }
 
     //For a point
-    inBoundsPoint (x, y) {
-        return (x >= this.xMin && x < this.xMax) && (y >= this.yMin && y < this.yMax);
+    inBoundsPoint (point) {
+        let x = point.xMin;
+        let y = point.yMin;
+        return (x >= this.xMin && x <= this.xMax) && (y >= this.yMin && y <= this.yMax);
     }
 
-    //For another AABB
-    inBoundsAABB (bounds) {
+    //For another Box
+    inBoundsBox (bounds) {
         if (this.xMax < bounds.xMin) {return false}
-        if (this.xMin > bounds.xMax) {return false}
+        if (this.xMin >= bounds.xMax) {return false}
         if (this.yMax < bounds.yMin) {return false}
         return this.yMin <= bounds.yMax;
     }
 
-    inBoundsCircle (circle) {
+    inBoundsCircle (bounds) {
         //TODO
     }
 
-    inBounds (bounding) {
-        switch (bounding.type) {
-            case 'box': return this.inBoundsAABB(bounding);
-            //case 'circle': return this.inBoundsCircle(bounding);
-            default:
-                //If now bounding, treat as a point
-                if (typeof bounding.x !== 'undefined' && typeof bounding.y !== 'undefined') {
-                    return this.inBoundsPoint(bounding.x, bounding.y);
-                }
-        }
+    //Wrapper function
+    inBounds (bounds) {
+        return $IN_BOUNDS(this, bounds);
     }
 
 }
 
 //CIRCLE TODO This needs a rewrite
-/*class BoundingCircle {
+class Circle {
 
     //x, y from top left corner
     constructor (x, y, radius) {
@@ -102,13 +151,13 @@ class Box {
         return dist <= this.radius + circle.radius;
     }
 
-    inBoundsAABB (AABB) {
+    inBoundsBox (Box) {
         //TODO
     }
 
     inBounds (bounding) {
         switch (bounding.type) {
-            case 'aabb': return this.inBoundsAABB(bounding);
+            case 'Box': return this.inBoundsBox(bounding);
             case 'circle': return this.inBoundsCircle(bounding);
             default:
                 if (typeof bounding.x !== 'undefined' && typeof bounding.y !== 'undefined') {
@@ -116,14 +165,13 @@ class Box {
                 }
         }
     }
-}*/
-
-//LINES
-//TODO Lines
+}
 
 //ALL BOUNDING
 const $BOUNDS = {
-    box: Box//,
+    box: Box,
+    point: Point,
+    //line: Line
     //circle: BoundingCircle
 };
 
@@ -132,44 +180,110 @@ const $IS_BOUNDS = function (bounds) {
     return $BOUNDS[bounds.type] !== undefined;
 };
 
-//CAST TESTING
-//A general test for possible intersection of bounds by casting
-//Elements refers to an array of all bounds being tested
-//Casts and checks x coordinates
-//Returns md array of all intersections
-const $CAST = function (elements, specific) {
-    let intersections = []; //Array of arrays of intersecting bounds
-    let active = []; //Active bounds
-    let cast = []; //Cast start and end points of the bounds
+//TESTS FOR COLLISIONS
+//The entities aren't bounds themselves, but contain a bounds property
+//Does both broad and narrow phases
+//Entities should contain entity for specific collision checks
+const $COLLISIONS = function (entities, specific) {
 
-    //Cast all points down
-    for (let i = 0; i < elements.length; i++) {
-        let bounds;
-        //For bounds
-        if ($IS_BOUNDS(elements[i])) {
-            bounds = elements[i];
+    //BROAD PHASE
+    //Map entities to x axis with start and stop points
+    let map = [];
+    //Iterates over entities and orders in map
+    for (let i = 0; i < entities.length; i++) {
+
+        //Start and end points
+        let start = {entity: entities[i], x: entities[i].bounds.xMin, start: true};
+        let end   = {entity: entities[i], x: entities[i].bounds.xMax, start: false};
+
+        //Iterate over - O(nlogn)
+        let points = [start, end];
+        for (let j = 0; j < 2; j++) {
+            //Since map will be sorted, use binary search
+            let floor = -1;
+            let ceil = map.length;
+            let inserted = false;
+            while (!inserted) {
+                let index = Math.floor((ceil + floor)/2);
+                //Found insertion index
+                if (ceil - floor === 1) {
+                    map.splice(ceil, 0, points[j]);
+                    inserted = true;
+                }
+                else if (map[index].x === points[j].x) {
+                    map.splice(index, 0, points[j]);
+                    inserted = true;
+                }
+                //Move search area
+                else if (map[index].x > points[j].x) {
+                    ceil = index;
+                }
+                else if (map[index].x < points[j].x) {
+                    floor = index;
+                }
+            }
         }
-        //For entity
-        else if (elements[i].bounds !== undefined) {
-            bounds = elements[i].bounds;
-        }
-        //For a single point
-        else if (elements[i].x !== undefined && elements[i].y !== undefined) {
-            bounds = {xMin: x, xMax: x};
-        }
-        //If can be cast
-        if (bounds.xMin !== undefined && bounds.xMax !== undefined) {
-            //TODO Need a way of casting and checking for intersection that fairly simple
-        }
-        //Else just continue and ignore it
     }
 
-    //If looking for general intersections
+    let collisions = [];
     if (specific === undefined) {
+
+        //For nonspecific cases
+        //Initial collisions from map
+        let active = [];
+        for (let i = 0; i < map.length; i++) {
+            let current = map[i].entity;
+            if (map[i].start) {
+                //Add collision pairs
+                if (current === entity || entity === undefined) {
+                    for (let j = 0; j < active.length; j++) {
+                        //Exclude chunk chunk collisions
+                        //TODO Temp fix, find better solution
+                        if (!(current.type === 'chunk' && active[j].type === 'chunk')) {
+                            //NARROW PHASE
+                            if (current.bounds.inBounds(active[j].bounds)) {
+                                collisions.push([current, active[j]]);
+                            }
+                        }
+
+                    }
+                    active.push(current);
+                }
+            }
+            //Remove when ends
+            else {
+                let index = active.indexOf(current);
+                if (index > -1) {
+                    active.splice(index, 1);
+                }
+            }
+        }
+
     }
+    //If one set collides with another
+    else {
+
+        for (let i = 0; i < specific.length; i++) {
+            let entity = specific[i];
+            //linear search for bounds
+            for (let j = 0; j < map.length; j++) {
+                if(map[j].start) {
+                    //NARROW PHASE
+                    if(entity.bounds.inBounds(map[j].entity.bounds)) {
+                        collisions.push([map[j].entity, entity]);
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    return collisions;
+
 };
 
 //EXPORTS
 exports.bounds = $BOUNDS;
 exports.isBounds = $IS_BOUNDS;
-exports.cast = $CAST;
+exports.getCollisions = $COLLISIONS;
