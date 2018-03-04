72b4b73e6d1e12eb7e3d7c6d7c08ac7781386e5a
henrywinchester25565 <henrywinchester25565@googlemail.com>
2018-03-04 16:46:28 +0000

    Players are 2/3rds the old size.

--- src/entity.js
+++ src/entity.js
@@ -345,7 +345,7 @@ class Player extends Physics {
 
     constructor (x, y, r) {
         super(x, y);
-        this.radius = r || 0.3 * 3;
+        this.radius = r || 0.6;
 
         //A player with radius 0.4 is considered average,
         //So is given the average mass of an adult male.
